49d940f3524a8b15b545e8f7e65e4b03c3ef24ee
denisceno <dceno88@gmail.com>
2024-09-10 00:01:07 +0200

    us_states_game fixes

--- US_States_Game/main.py
+++ US_States_Game/main.py
@@ -1,5 +1,6 @@
 import turtle
 import pandas as pd
+import time
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -7,18 +8,28 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 data = pd.read_csv("50_states.csv")
-data['state'] = data['state'].str.lower()
+all_states = data['state'].to_list()
 
-s_count = 0
-while True:
-    answer = screen.textinput(title=f"Guess the state {s_count}/50", prompt="Try finding a state").lower()
+correct_a = []
+while len(correct_a) < 50:
+    answer = screen.textinput(title=f"Guess the state {len(correct_a)}/50", prompt="Try finding a state").title()
+    if answer == "Exit":
+        not_found_states = []
+        for x in all_states:
+            if x not in correct_a:
+                not_found_states.append(x)
+        new_csv = pd.DataFrame(not_found_states)
+        new_csv.to_csv("States_to_learn.csv")
+
+        break
+
     t1 = turtle.Turtle()
     t1.hideturtle()
     t1.penup()
     if answer in data["state"].values:
+        correct_a.append(answer)
         row = data[data["state"] == answer]
         x = row["x"].values[0]
         y = row["y"].values[0]
         t1.goto(x, y)
-        t1.write(answer.title(), align="center", font=("Arial", 16, "normal"))
-        s_count += 1
+        t1.write(answer.title())
